d5a0a6f87cdf33d48c5bb1a796a9a7eb951854f6
Michel Marques <michelmarques.aluno@unipampa.edu.br>
2026-01-13 16:59:14 -0300

    Configurar GitHub Actions para deploy automático

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Para repositório username.github.io, use '/'
-  // Para repositório de projeto, use '/nome-do-repositorio/'
-  base: '/',
+  base: '/servicos-gerais.github.io/',
 })
